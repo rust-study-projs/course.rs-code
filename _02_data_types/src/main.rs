97fff69cdbcf70b175649541e3673165d46bc664
jaychenfe <chenjiehwy@163.com>
2022-02-19 21:34:45 +0800

    init

---
+++ _02_data_types/src/main.rs
@@ -0,0 +1,44 @@
+// fn main() {
+//     // 编译器会进行自动推导，给予twenty i32的类型
+//     let twenty = 20;
+//     // 类型标注
+//     let twenty_one: i32 = 21;
+//     // 通过类型后缀的方式进行类型标注：22是i32类型
+//     let twenty_two = 22i32;
+
+//     // 只有同样类型，才能运算
+//     let addition = twenty + twenty_one + twenty_two;
+//     println!(
+//         "{} + {} + {} = {}",
+//         twenty, twenty_one, twenty_two, addition
+//     );
+
+//     // 对于较长的数字，可以用_进行分割，提升可读性
+//     let one_million: i64 = 1_000_000;
+//     println!("{}", one_million.pow(2));
+
+//     // 定义一个f32数组，其中42.0会自动被推导为f32类型
+//     let forty_twos = [42.0, 42f32, 42.0_f32];
+
+//     // 打印数组中第一个值，并控制小数位为2位
+//     println!("{:.2}", forty_twos[0]);
+// }
+
+
+fn main() {
+    let abc: (f32, f32, f32) = (0.1, 0.2, 0.3);
+    let xyz: (f64, f64, f64) = (0.1, 0.2, 0.3);
+
+    println!("abc (f32)");
+    println!("   0.1 + 0.2: {:x}", (abc.0 + abc.1).to_bits());
+    println!("         0.3: {:x}", (abc.2).to_bits());
+    println!();
+
+    println!("xyz (f64)");
+    println!("   0.1 + 0.2: {:x}", (xyz.0 + xyz.1).to_bits());
+    println!("         0.3: {:x}", (xyz.2).to_bits());
+    println!();
+
+    assert!(abc.0 + abc.1 == abc.2);
+    assert!(xyz.0 + xyz.1 == xyz.2);
+}
---
+++ _04_variables/src/main.rs
@@ -0,0 +1,32 @@
+// 变量可变性
+// fn main() {
+//     // let x = 5;
+//     let mut x = 5;
+//     println!("The value of x is: {}", x);
+//     x = 6;
+//     println!("The value of x is: {}", x);
+// }
+
+// 变量解构
+// fn main() {
+//     let (a, mut b): (bool, bool) = (true, false);
+//     // a = true,不可变; b = false，可变
+//     println!("a = {:?}, b = {:?}", a, b);
+
+//     b = true;
+//     assert_eq!(a, b);
+// }
+
+// shadowing
+// fn main() {
+//     let x = 5;
+//     // 在main函数的作用域内对之前的x进行遮蔽
+//     let x = x + 1;
+//     {
+//         // 在当前的花括号作用域内，对之前的x进行遮蔽
+//         let x = x * 2;
+//         println!("The value of x in the inner scope is: {}", x);
+//     }
+
+//     println!("The value of x is: {}", x);
+// }
