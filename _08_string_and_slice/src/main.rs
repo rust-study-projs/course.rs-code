2ddd2d8ef8428aec97707ca73ff3b8d7438b68ce
jaychenfe <chenjiehwy@163.com>
2022-02-21 18:42:34 +0800

    2

---
+++ _08_string_and_slice/src/main.rs
@@ -0,0 +1,21 @@
+// fn main() {
+//     let my_name = "Pascal";
+//     greet(my_name);
+//   }
+
+//   fn greet(name: String) {
+//     println!("Hello, {}!", name);
+//   }
+
+// fn main() {
+//     let mut s = String::from("hello world");
+
+//     let word = first_word(&s);
+
+//     s.clear(); // error!
+
+//     println!("the first word is: {}", word);
+// }
+// fn first_word(s: &String) -> &str {
+//     &s[..1]
+// }
